3198e7ad97377d310ccf80d50fdf6efbc0c6c90f
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2025-02-21 17:28:35 +0700

    chore(svelte): add some aliases
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- svelte.config.js
+++ svelte.config.js
@@ -8,7 +8,13 @@ const config = {
   preprocess: vitePreprocess(),
 
   kit: {
-    adapter: adapter()
+    adapter: adapter(),
+    alias: {
+      $components: 'src/lib/components',
+      $helpers: 'src/lib/helpers',
+      $hooks: 'src/lib/hooks',
+      $types: 'src/lib/types.d.ts'
+    }
   }
 };
 
