84de07ae5758b1d3f5b1d3b999deb6640e289df3
Kiizuha Kanazawa <kiizuha@gnuweeb.org>
2025-02-21 18:12:07 +0700

    draft!: draft work WIP, urgent!
    Signed-off-by: Kiizuha Kanazawa <kiizuha@gnuweeb.org>

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,2 +1,50 @@
-<h1>Welcome to SvelteKit</h1>
-<p>Visit <a href="https://svelte.dev/docs/kit">svelte.dev/docs/kit</a> to read the documentation</p>
+<script lang="ts">
+  import { browser } from '$app/environment';
+  import { preloadAssets } from '$helpers/loader';
+  import { ASSETS } from '$lib/constants';
+  import type { MediaElementState, MediaPlayState } from '$types';
+  import { onMount } from 'svelte';
+
+  let media = $state<MediaElementState>();
+  let playState = $state<MediaPlayState>();
+
+  const startLoadAssets = () => {
+    if (!screen.orientation.type.includes('landscape')) return;
+    preloadAssets({ assets: ASSETS });
+  };
+
+  onMount(() => {
+    console.log('onMount');
+    if (!browser || !media || !playState) return;
+
+    console.log('startLoadAssets start');
+    startLoadAssets();
+    console.log('startLoadAssets end');
+
+    media.videoIntro.onended = () => {
+      media.videoIntro.oncanplay = null;
+      media.videoIntro.classList.remove('z-[1]');
+
+      media.videoLoop.classList.remove('z-[-1]');
+      media.videoLoop.classList.add('z-[1]');
+      media.videoLoop.play();
+
+      setTimeout(() => {
+        // use timeout for letting the looped video play first.
+        media.videoIntro.remove();
+      }, 1000);
+
+      playState.canPlay = true;
+    };
+  });
+</script>
+
+<video
+  bind:this={media?.videoIntro}
+  preload="auto"
+  class="absolute top-0 left-0 z-[1] h-screen
+          w-screen object-cover brightness-[80%]"
+>
+  <track kind="captions" />
+  <source type="video/webm" />
+</video>
